d66978fc0651da9432a210574dbc7dd8b1b3723f
Santiago Grela Mata <grela.mata.santiago@gmail.com>
2022-02-14 19:17:44 +0100

    changed db access data, added model module and saved database diagram and creation script

---
+++ backend/model/src/main/resources/db_creation.sql
@@ -0,0 +1,53 @@
+drop database if exists myvideogamelist;
+create database myvideogamelist;
+use myvideogamelist;
+
+create table video_games(
+    id integer primary key auto_increment,
+    title varchar(50) not null,
+    release_date date
+);
+
+create table genres(
+    id integer primary key auto_increment,
+    name varchar(50)
+);
+
+create table companies(
+    id integer primary key auto_increment,
+    name varchar(50) not null,
+    foundation_date date not null,
+    localization varchar(50) not null
+);
+
+create table platforms(
+    id integer primary key auto_increment,
+    name varchar(50) not null,
+    release_date date not null,
+    developer integer not null,
+    foreign key (developer) references companies(id)
+);
+
+create table video_game_genre(
+    video_game_id integer,
+    genre_id integer,
+    foreign key (video_game_id) references video_games(id),
+    foreign key (genre_id) references genres(id),
+    primary key(video_game_id, genre_id)
+);
+
+create table video_game_platform(
+    video_game_id integer,
+    platform_id integer,
+    foreign key (video_game_id) references video_games(id),
+    foreign key (platform_id) references platforms(id),
+    primary key(video_game_id, platform_id)
+);
+
+create table video_game_company(
+    video_game_id integer,
+    company_id integer,
+    foreign key (video_game_id) references video_games(id),
+    foreign key (company_id) references companies(id),
+    primary key (video_game_id, company_id)
+);
